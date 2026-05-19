05846188211e45537b523cd639e83725ea6a2ab1
riantosee <riantosee@gmail.com>
2026-05-19 22:22:19 +0700

    Update page.js

--- app/tester/page.js
+++ app/tester/page.js
@@ -47,7 +47,7 @@ const CATEGORIES = [
         items   : [
           { label: 'Home',        method: 'GET', path: '/api/anime/home-sanka',  params: '' },
           { label: 'Ongoing',method: 'GET', path: '/api/anime/ongoing',  params: 'page=1' },
-          { label: 'Popular',       method: 'GET', path: '/api/v2/anime/popular', params: '' },
+          { label: 'Complete Anime',       method: 'GET', path: '/api/anime/complete-sanka', params: '' },
           { label: 'Detail',        method: 'GET', path: '/api/v2/anime/detail',  params: 'slug=one-punch-man' },
           { label: 'Watch',         method: 'GET', path: '/api/v2/anime/watch',   params: 'slug=one-punch-man-episode-12' },
         ],
